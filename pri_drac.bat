b23f85b792968fc6efd843f2494865ebce091fa9
tery chinz <chinz0020@gmail.com>
2022-01-29 03:23:15 +0700

    up hydra

---
+++ pri_drac.bat
@@ -0,0 +1,1 @@
+node "D:\vscode_mir4\test.js"
--- test.js
+++ test.js
@@ -1,10 +1,16 @@
 const axios = require('axios');
 const qs = require("qs");
 var cron = require('node-cron');
+var thb_us = 32.82;
+var HydraPriceWemix;
 require('dotenv').config({ path: '.env' })
 
+function getBaseLog(x, y) {
+    return Math.log(y) / Math.log(x);
+  }
 
-const alertfunc = (alertnow = false) => {
+
+const alertfunc = async (alertnow = false) => {
 
     let config = {
         method: 'post',
@@ -16,14 +22,81 @@ const alertfunc = (alertnow = false) => {
         }
     };
 
+    let confighydra = {
+        method: 'post',
+        url: 'https://api.mir4global.com/wallet/prices/hydra/lastest',
+        headers: { 
+            'Accept': 'application/json, text/plain, */*',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+            'Referer': 'https://www.mir4draco.com/'
+        },
+        };
+
+        let getEGG = {
+            method: 'post',
+            url: 'https://api.mir4global.com/wallet/prices/hydra',
+            headers: { 
+                'Accept': 'application/json, text/plain, */*',
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+                'Referer': 'https://www.mir4draco.com/'
+            },
+            };
+
+
+        
+         let HydraPriceWemix,totlesupply_hydra;
+        try {
+            let resulthtdra =  await axios(confighydra)
+
+            let resultcalEgg_hydra = await axios(getEGG);
+            let arrEgg = resultcalEgg_hydra.data.Data;
+            totlesupply_hydra = arrEgg[arrEgg.length-1].TotalSupply;
+            let anshydra = JSON.parse(JSON.stringify(resulthtdra.data))
+            // console.log(anshydra)
+            HydraPriceWemix = parseFloat(anshydra.Data.HydraPriceWemix).toFixed(4)
+        } catch (error) {
+            HydraPriceWemix = "Hydra Error.";
+        }
+
+
+               
+        let state = parseInt(totlesupply_hydra/100000)+1;
+        let arr = [];
+        
+        let sum = 0;
+        
+for (let i = 1; i <= state; i++) {
+// Sumation
+let algrolitum =  0.6 / parseFloat(getBaseLog(3.7,i+1));
+// console.log(i)
+// console.log(algrolitum)
+arr.push(algrolitum)
+// console.log(algrolitum)
+}
+    // console.log(arr)
+const reducer = (accumulator, curr) => accumulator + curr;
+let useDracobuthydra = 20 + Math.round(arr.reduce(reducer))
+
+
+    
+
     axios(config)
         .then((response) => {
             let ans = JSON.parse(JSON.stringify(response.data))
-            let thb_us = 32.82
+
             let DrapriceTHB = ans.Data.USDDracoRate * thb_us;
             let Wemixone_THB = ans.Data.USDWemixRate * thb_us;
-            let alerttoline = `Draco-THB: ${parseFloat(DrapriceTHB).toFixed(3)}\n\rWemix-THB: ${parseFloat(Wemixone_THB).toFixed(3)}\n\rDraco-ExchageWemix: ${parseFloat(ans.Data.DracoPrice).toFixed(3)}`;
+            let DracoPriceWemix = parseFloat(ans.Data.DracoPrice).toFixed(4)
+            let dragobuyHydra = parseFloat(DracoPriceWemix*useDracobuthydra).toFixed(4);
 
+      
+            let alerttoline = `\n\rDraco-THB: ${parseFloat(DrapriceTHB).toFixed(3)}\n\rWemix-THB: ${parseFloat(Wemixone_THB).toFixed(3)}\n\rDraco-ExchageWemix: ${DracoPriceWemix}\n\rDraco-Buy_Hydra : ${dragobuyHydra} wemix\n\r\n\r======= Hrdra ======= \n\rหลอม  ${useDracobuthydra} Draco + ${state} Egg\n\rHydraPriceWemix : ${HydraPriceWemix}\n\rTotal_Token : ${totlesupply_hydra}\n\rState : ${state}`;
+
+
+            // cal
+            // console.log( Math.log10(37100));
+
+            // console.log(algrolitum)
             console.log(alerttoline)
             let bodydata = qs.stringify({
                 message: alerttoline
@@ -76,7 +149,7 @@ console.log("Start ing...." + process.env.tokenline);
 alertfunc();
 cron.schedule('*/30 * * * *', () => {
 
-    alertfunc(30);
+    // alertfunc(30);
 });
 
 cron.schedule('*/10 * * * *', () => {
